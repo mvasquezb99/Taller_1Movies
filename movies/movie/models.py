be86802e35290c31229e8fd328747844be7e2395
mvasquezb99 <mvasquezb@eafit.edu.co>
2023-08-04 10:39:22 -0500

    Visualizacion de peliculas

--- movies/movie/models.py
+++ movies/movie/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    url = models.URLField(blank=True)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank= True)
--- movies/movie/views.py
+++ movies/movie/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import Movie
 
 # Create your views here.
 
 
 def Home(request):
-    return render(request, 'index.html', {'name': 'Miguel Vasquez Bojanini'})
+    #return render(request, 'index.html', {'name': 'Miguel Vasquez Bojanini'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__contains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'index.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def About(request):
     return render(request, 'details.html')
